1a56327588e6f714cb5085c25be683bd9fbb85c6
Sonia Batllori <sbatllori@hotmail.com>
2021-04-13 23:51:04 +0200

    add(draw edge normal)

--- p5js_sketches/connected dots/sketch.js
+++ p5js_sketches/connected dots/sketch.js
@@ -4,8 +4,8 @@ const palette1 = ['#bfd9d7', '#ddbd4d', '#4c807c', '#987239', '#353745'];
 const palette2 = ['#f8e6d1', '#f39f87', '#f6af1a', '#978e91', '#36513a'];
 
 let bEdge = true;
-const edgeColors = palette1;
 let edgeColorIdx = 0;
+const edgeColors = palette2;
 
 let savedPoints = []; // [x y bEdge edgeColorIdx]
 
@@ -19,15 +19,34 @@ function draw() {
 
   let mouse = [mouseX, mouseY, bEdge, edgeColorIdx];
   let points = savedPoints.concat([mouse]);
+  if (mouseIsPressed) savedPoints.push(mouse);
 
+  // Draw edges
   strokeWeight(15);
   R.aperture(2, points).forEach(([p, q]) => {
     if (q[2]) {
       stroke(edgeColors[q[3]]);
       line(p[0], p[1], q[0], q[1]);
+
+      // Draw a normal line in the middle of the edge
+      // - compute direction P---->Q
+      // - compute middle point P--x--Q
+      // - compute normal vector
+      let direction = createVector(q[0] - p[0], q[1] - p[1]);
+      let middelPoint = [p[0] + direction.x / 2, p[1] + direction.y / 2];
+      direction.rotate(HALF_PI).normalize();
+
+      const normalLength = 50;
+      line(
+        middelPoint[0],
+        middelPoint[1],
+        middelPoint[0] + normalLength * direction.x,
+        middelPoint[1] + normalLength * direction.y
+      );
     }
   });
 
+  // Draw points
   strokeWeight(0);
   points.forEach(([x, y]) => {
     fill(0);
@@ -36,8 +55,6 @@ function draw() {
     fill(255);
     circle(x, y, 50);
   });
-
-  if (mouseIsPressed) savedPoints.push(mouse);
 }
 
 function keyPressed() {
