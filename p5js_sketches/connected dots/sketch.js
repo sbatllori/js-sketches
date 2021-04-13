2466cdc48762014be0d47eb189f8b4ad6e70952d
Sonia Batllori <sbatllori@hotmail.com>
2021-04-13 22:28:37 +0200

    add(toggle edge drawing and change edge color)

--- p5js_sketches/connected dots/sketch.js
+++ p5js_sketches/connected dots/sketch.js
@@ -1,9 +1,13 @@
 // https://www.color-hex.com/color-palette/107816
-// const palette1 = ['#bfd9d7', '#ddbd4d', '#4c807c', '#987239', '#353745'];
+const palette1 = ['#bfd9d7', '#ddbd4d', '#4c807c', '#987239', '#353745'];
 // https://www.color-hex.com/color-palette/95230
-// const palette2 = ['#f8e6d1', '#f39f87', '#f6af1a', '#978e91', '#36513a'];
+const palette2 = ['#f8e6d1', '#f39f87', '#f6af1a', '#978e91', '#36513a'];
 
-let saved_points = [];
+let bEdge = true;
+const edgeColors = palette1;
+let edgeColorIdx = 0;
+
+let savedPoints = []; // [x y bEdge edgeColorIdx]
 
 function setup() {
   createCanvas(1080, 1080);
@@ -13,13 +17,15 @@ function setup() {
 function draw() {
   background(50);
 
-  let mouse = [mouseX, mouseY];
-  let points = saved_points.concat([mouse]);
+  let mouse = [mouseX, mouseY, bEdge, edgeColorIdx];
+  let points = savedPoints.concat([mouse]);
 
-  stroke(0);
   strokeWeight(15);
   R.aperture(2, points).forEach(([p, q]) => {
-    line(p[0], p[1], q[0], q[1]);
+    if (q[2]) {
+      stroke(edgeColors[q[3]]);
+      line(p[0], p[1], q[0], q[1]);
+    }
   });
 
   strokeWeight(0);
@@ -31,11 +37,15 @@ function draw() {
     circle(x, y, 50);
   });
 
-  if (mouseIsPressed) saved_points.push(mouse);
+  if (mouseIsPressed) savedPoints.push(mouse);
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function keyPressed() {
+  if (keyCode === 'E'.charCodeAt(0)) {
+    bEdge = !bEdge;
+  }
+
+  if (keyCode === 'C'.charCodeAt(0)) {
+    edgeColorIdx = (edgeColorIdx + 1) % edgeColors.length;
+  }
 }
