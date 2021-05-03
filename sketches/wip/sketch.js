f86f4d7214976ab817b24f236d377c5312cc0ff5
Sonia Batllori <sbatllori@hotmail.com>
2021-05-03 22:55:10 +0200

    add(customize tree and leafs; wip moon)

--- sketches/wip/sketch.js
+++ sketches/wip/sketch.js
@@ -1,8 +1,8 @@
 // wip
 
 let bgColor;
-let trunkLen;
-const maxTrunkLen = 200;
+const minBranchLen = 10;
+const maxBranchLen = 200;
 
 const capture = false;
 SOO_LAST_CAPTURED_FRAME = 2000;
@@ -11,6 +11,7 @@ function setup() {
   createCanvas(1080, 1080, WEBGL);
   frameRate(fps);
   angleMode(DEGREES);
+
   bgColor = color(220);
 }
 
@@ -18,63 +19,101 @@ function draw() {
   if (capture) preCapture();
 
   background(bgColor);
-  randomSeed(1);
 
-  rotateY(0.5 * frameCount);
-  scale(2);
-  translate(0, maxTrunkLen, 0);
-  trunkLen = map(mouseX, 0, width, 0, maxTrunkLen);
-  branch(trunkLen);
+  noFill();
+  stroke(22);
+  strokeWeight(2);
+  line(-500, 200, 500, 200);
+
+  const r = 0.4 * width;
+  const x = r * cos(frameCount + 180);
+  const y = r * sin(-frameCount);
+  circle(x, y, 100);
+
+  scale(1.5);
+  randomSeed(1);
+  // drawTree(200, 0, 250, 0);
 
   if (capture) postCapture();
 }
 
-function branch(len) {
-  strokeWeight(0);
-  fill(43, 33, 30);
+function branchRecursive(len) {
+  drawBranch(len);
 
-  let coneRadius = map(len, 0, maxTrunkLen, 1, 15);
-
-  push();
-  rotateZ(180);
-  translate(0, 0.5 * len, 0);
-  cone(coneRadius, len, 7, 7);
-  pop();
-
-  push();
-  translate(0, -0.5 * len, 0);
-  cone(0.5 * coneRadius, len, 7, 7);
-  pop();
-
-  if (len > 15) {
+  if (len > minBranchLen) {
     translate(0, -len, 0);
 
-    let numChildBranches = map(mouseY, 0, height, 7, 2);
-    let childBranchLen = map(mouseX, 0, width, 0.1, 0.7) * len;
-
-    for (let i = 0; i < numChildBranches; i++) {
-      rotateY(360 / numChildBranches);
-      rotateY(random(0, 40));
+    for (let i = 0; i < 3; i++) {
+      rotateY(360 / 3);
+      rotateY(random(-30, 30));
 
       push();
-      rotateZ(180 / numChildBranches);
-      rotateZ(random(-10, 30));
-      branch(childBranchLen);
+      rotateZ(45);
+      rotateZ(random(-10, 10));
+      branchRecursive(0.62 * len);
       pop();
     }
   } else {
-    leaf(len);
+    drawLeafs(len);
   }
 }
 
-function leaf(len) {
-  stroke(22, 100);
-  strokeWeight(2);
-  fill(50, 156, 138);
-  // fill(255, 244, 163);
-
+function drawTree(len, x, y, z) {
   push();
-  translate(0, -1.25 * len, 0);
-  ellipse(0, 0, 0.5 * len, len);
+  {
+    translate(x, y, z);
+    branchRecursive(len);
+  }
   pop();
+}
+
+function drawBranch(len) {
+  strokeWeight(len < 40 ? 0.05 * len : 4);
+  stroke(22);
+  fill(53, 43, 40);
+
+  const coneRadius = map(len, 0, maxBranchLen, 1, 15);
+  push();
+  {
+    rotateZ(180);
+    translate(0, 0.5 * len, 0);
+    cone(coneRadius, len, 7, 1);
+  }
+  pop();
+  push();
+  {
+    translate(0, -0.5 * len, 0);
+    cone(0.5 * coneRadius, len, 7, 1);
+  }
+  pop();
+}
+
+function drawLeafs(len) {
+  strokeWeight(0.8);
+  fill(50, 196, 138);
+
+  translate(0, -1.8 * len, 0);
+  const r = len;
+
+  beginShape();
+  for (let i = 0; i <= 90; ++i) {
+    vertex(r * cos(i), r * sin(i));
+  }
+  for (let i = 180; i <= 270; ++i) {
+    vertex(r * cos(i) + r, r * sin(i) + r);
+  }
+  endShape(CLOSE);
+
+  rotateZ(90);
+  randomSeed(null);
+  rotateX(random(-20, 20));
+  randomSeed(1);
+  beginShape();
+  for (let i = 0; i <= 90; ++i) {
+    vertex(r * cos(i), r * sin(i));
+  }
+  for (let i = 180; i <= 270; ++i) {
+    vertex(r * cos(i) + r, r * sin(i) + r);
+  }
+  endShape(CLOSE);
 }
